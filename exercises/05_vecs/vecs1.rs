63065d3eacd0a06c589dd820da25a6a9e3024bc4
thiagopls1 <thiagopls1@hotmail.com>
2025-07-22 18:14:25 -0300

    Do `primitive_types` and `vecs` exercises

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -3,13 +3,16 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
 
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
-    // let v = ???;
+    let v = a.to_vec();
 
     (a, v)
 }
 
 fn main() {
     // You can optionally experiment here.
+    let (a, v) = array_and_vec();
+    println!("{:#?}", v);
+    println!("{:#?}", a);
 }
 
 #[cfg(test)]
